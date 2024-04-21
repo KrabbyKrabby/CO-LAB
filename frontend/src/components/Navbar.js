678cb4eb8ab662022ac59502068883a1f134aa62
Ittehad Saleh Chowdhury <ittehad@Ittehads-MacBook-Air.local>
2024-04-22 02:34:35 +0600

    added navigation

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,25 +1,27 @@
 import React from 'react'
 import './CSS/Navbar.css'
 import logo from '../assets/colab_logo.png'
-
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+
   return (
     <nav className='container'>
-      <img src= {logo} alt="" className='logo'/>
+      <img src={logo} alt="" className='logo'/>
       <ul>
-        <li><button className='btn'>Home</button></li>
-        <li><button className='btn'>Community</button></li>
-        <li><button className='btn'>Profile</button></li>
-        <li><button className='btn'>Contact Us</button></li>
+        <li><button className='btn' onClick={() => navigate('/')}>Home</button></li>
+        <li><button className='btn' >Community</button></li>
+        <li><button className='btn' onClick={() => navigate('/profile')}>Profile</button></li>
+        <li><button className='btn' >Contact Us</button></li>
       </ul>
 
       <ul>
-        <li><button className='btn'>LogIn</button></li>
-        <li><button className='btn'>Sign Up</button></li>
+        <li><button className='btn' onClick={() => navigate('/login')}>LogIn</button></li>
+        <li><button className='btn' onClick={() => navigate('/signup')}>Sign Up</button></li>
       </ul>
     </nav>
-  )
+  );
 }
 
 export default Navbar
